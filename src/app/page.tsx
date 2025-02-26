cae64cc13318ffe5f987209646f4fae9b00e5cba
gelo <gelodev83@gmail.com>
2025-02-26 19:48:32 +0800

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Navbar from "@/components/Navbar/Navbar";
 import Image from "next/image";
 import { HomeSection } from "@/components/Sections/Home/Home";
 import FirstSection from "@/components/Sections/FirstSection";
+import SecondSection from "@/components/Sections/SecondSection";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <Navbar />
       <HomeSection />
       <FirstSection />
+      <SecondSection/>
       <div className="h-screen"></div>
     </div>
   );
---
+++ src/components/Sections/SecondSection.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function SecondSection() {
+  return (
+    <div className='h-screen bg-gray-100'></div>
+  )
+}
