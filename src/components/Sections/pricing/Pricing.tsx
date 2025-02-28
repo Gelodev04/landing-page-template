91ba578fd6e99725ceb8b8e534660970a03c8dba
gelo <gelodev83@gmail.com>
2025-02-28 09:06:36 +0800

    updated

--- src/components/Sections/pricing/Pricing.tsx
+++ src/components/Sections/pricing/Pricing.tsx
@@ -1,13 +1,10 @@
 import React from "react";
 import { fetchPricingSection } from "@/utils/api";
+import { Arrow } from "./icon";
 
 export default async function Pricing() {
   const data = await fetchPricingSection();
   const pricingSectionData = data?.data;
-
-  if (!pricingSectionData) {
-    return <div>Loading...</div>;
-  }
 
   return (
     <section className="mt-[4rem] font-inter">
@@ -23,31 +20,47 @@ export default async function Pricing() {
 
         <div className="grid md:grid-cols-3 gap-6">
           {pricingSectionData.card.card.map((plan: any, index: number) => (
-            <div key={index} className="p-6 border rounded shadow-md bg-white min-h-[500px] flex flex-col ">
-              <div className="flex-grow">
-                  <div className="border-b pb-1">
-                    <h3 className="text-xl text-[#1E293B] font-semibold">{plan.title}</h3>
-                    <p className="text-4xl  my-2">
-                      <span className="text-[#64748B] ">$</span>
-                      <span className="text-[#1E293B] font-playfair font-semibold">
-                        {plan.price}
-                      </span>
-                      <span className="text-base text-[#94A3B8]">/mo</span>
-                    </p>
-                    <p className="text-[#94A3B8] mb-4">{plan.description}</p>
-                  </div>
+            <div
+              key={index}
+              className="p-6 border rounded shadow-md bg-white min-h-[500px] flex flex-col relative"
+            >
+              {index === 1 && (
+                <div className="absolute top-0 left-[20rem] -translate-x-1/2 -translate-y-1/2 bg-[#A7F3D0] text-[#047857] px-4 py-1  rounded-full text-sm font-semibold whitespace-nowrap">
+                  Most Popular
+                </div>
+              )}
 
-                  <ul className="text-gray-500 space-y-4">
+              <div className="flex-grow">
+                <div className="border-b pb-1">
+                  <h3 className="text-xl text-[#1E293B] font-semibold">
+                    {plan.title}
+                  </h3>
+                  <p className="text-4xl  my-2">
+                    <span className="text-[#64748B] ">$</span>
+                    <span className="text-[#1E293B] font-playfair font-semibold">
+                      {plan.price}
+                    </span>
+                    <span className="text-base text-[#94A3B8]">/mo</span>
+                  </p>
+                  <p className="text-[#94A3B8] mb-4">{plan.description}</p>
+                </div>
+
+                <ul className="text-gray-500 space-y-4">
                   <h4 className="text-[#1E293B] mt-3">{plan.features.title}</h4>
-                    {plan.features.list.map((feature: any, i: number) => (
-                      <li key={i} className="text-">
-                        ✅ {feature}
-                      </li>
-                    ))}
-                  </ul>
+                  {plan.features.list.map((feature: any, i: number) => (
+                    <li key={i} className="text-">
+                      ✅ {feature}
+                    </li>
+                  ))}
+                </ul>
               </div>
-            
-              <button className="bg-[#1D4ED8] text-white py-2 px-5 rounded w-full mt-5">Start free trial</button>
+
+              <button className="bg-[#1D4ED8] text-white py-2 px-5 rounded w-full mt-5 flex items-center justify-center gap-1">
+                <span>Start free trial </span>
+                <span>
+                  <Arrow />
+                </span>
+              </button>
             </div>
           ))}
         </div>
