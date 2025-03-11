0aafa1ac6a958a0ac67dcc95117763b57f2e20bb
gelo <gelodev83@gmail.com>
2025-03-11 11:18:20 +0800

    added animations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
-import Navbar from "@/components/Navbar/Navbar";
-import Image from "next/image";
+
 import { HomeSection } from "@/components/Sections/home/home_section/Home";
 import FirstSection from "@/components/Sections/home/first_section/FirstSection";
 import SecondSection from "@/components/Sections/home/second_section/SecondSection";
@@ -7,8 +6,7 @@ import ThirdSection from "@/components/Sections/home/third_section/ThirdSection"
 import { FourthSection } from "@/components/Sections/home/fourth_section/fourthSection";
 import FifthSection from "@/components/Sections/home/fifth_section/FifthSection";
 import Pricing from "@/components/Sections/home/pricing_section/Pricing";
-import Footer from "@/components/Sections/home/footer/Footer";
-import ScrollAnimation from "@/components/ui/ScrollAnimation";
+
 
 export default function Home() {
   return (
@@ -20,7 +18,7 @@ export default function Home() {
       <FourthSection />
       <FifthSection />
       <Pricing />
-      <ScrollAnimation/>
+      
     </div>
   );
 }
